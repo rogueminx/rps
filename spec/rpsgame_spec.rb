dad3d65312c0642a0b6ebb637471d2a42c02a0a3
Anna Marie Cooper and Hannah Lee <anna+hannah@>
2018-02-07 11:13:11 -0800

    allows for user input and a a computer generated player

--- spec/rpsgame_spec.rb
+++ spec/rpsgame_spec.rb
@@ -5,22 +5,22 @@ require ('pry')
 describe ('#game') do
   it ("returns true if rock is the object and scissors is the argument") do
     game = RPS.new()
-    expect(game.wins?("rock", "scissors")).to(eq(true))
+    expect(game.wins?("rock", "scissors")).to(eq("gamer1 wins"))
   end
   it ("returns true if scissors is the object and paper is the argument") do
     game = RPS.new()
-    expect(game.wins?("scissors", "paper")).to(eq(true))
+    expect(game.wins?("scissors", "paper")).to(eq("gamer1 wins"))
   end
   it ("returns true if paper is the object and rock is the argument") do
     game = RPS.new()
-    expect(game.wins?("paper", "rock")).to(eq(true))
+    expect(game.wins?("paper", "rock")).to(eq("gamer1 wins"))
   end
   it ("returns true if rock is the object and paper is the arguement") do
     game = RPS.new()
-    expect(game.wins?("scissors", "paper")).to(eq(true))
+    expect(game.wins?("scissors", "paper")).to(eq("gamer1 wins"))
   end
   it ("returns play again if object and argument are the same") do
     game = RPS.new()
-    expect(game.wins?("scissors", "scissors")).to(eq("play again"))
+    expect(game.wins?("scissors", "scissors")).to(eq("You tied! Play again"))
   end
 end
