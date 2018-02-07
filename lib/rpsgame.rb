1bcd8fd576500a6f4035ad43ca21e132c04f1ba3
Anna Marie Cooper and Hannah Lee <hannah@>
2018-02-07 10:04:11 -0800

    add two new rules to return true correctly

--- lib/rpsgame.rb
+++ lib/rpsgame.rb
@@ -1,11 +1,14 @@
 class RPS
   # def ()
-  #   @game
   # end
 
 
   def wins?(gamer1, gamer2)
     if (gamer1 == 'rock') & (gamer2 == 'scissors')
+      true
+    elsif (gamer1 == 'scissors') & (gamer2 == 'paper')
+      true
+    elsif (gamer1 == 'paper') & (gamer2 == 'rock')
       true
     end
   end
--- spec/rpsgame_spec.rb
+++ spec/rpsgame_spec.rb
@@ -4,7 +4,19 @@ require ('pry')
 
 describe ('#game') do
   it ("returns true if rock is the object and scissors is the argument") do
-     game = RPS.new()
-     expect(game.wins?("rock", "scissors")).to(eq(true))
+    game = RPS.new()
+    expect(game.wins?("rock", "scissors")).to(eq(true))
+  end
+  it ("returns true if scissors is the object and paper is the argument") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "paper")).to(eq(true))
+  end
+  it ("returns true if paper is the object and rock is the argument") do
+    game = RPS.new()
+    expect(game.wins?("paper", "rock")).to(eq(true))
+  end
+  it ("returns true if rock is the object and paper is the arguement") do
+    game = RPS.new()
+    expect(game.wins?("scissors", "paper")).to(eq(true))
   end
 end
